5dce84e3d09f34b93b161ef11faad4647d25dc5d
lyg0114 <whdnseowkd@naver.com>
2023-04-19 20:41:52 +0900

    refactoring

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,15 +5,16 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+  private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+  public App() {
+  }
 
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
+  public static void main(String[] args) {
+    System.out.println(MESSAGE);
+  }
 
-    public String getMessage() {
-        return MESSAGE;
-    }
+  public String getMessage() {
+    return MESSAGE;
+  }
 }
